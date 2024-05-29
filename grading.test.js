63f3fb4b111f94ecd43b9f2bd0369b08b5abc63f
AllanMuhari <allanmuhari087@gmail.com>
2024-05-29 16:11:32 +0300

    refactor

--- grading.test.js
+++ grading.test.js
@@ -5,15 +5,18 @@ describe("Test for grading", () => {
     expect(sciences(80)).toEqual("A");
     expect(sciences(65)).toEqual("B");
     expect(sciences(50)).toEqual("C");
+    expect(sciences(40)).toEqual("sup");
   });
   it("should return the correct grade for humanites", () => {
     expect(humanites(89)).toEqual("A");
     expect(humanites(70)).toEqual("B");
     expect(humanites(60)).toEqual("C");
+    expect(sciences(40)).toEqual("sup");
   });
   it("should return the correct grade for languages", () => {
     expect(languages(82)).toEqual("A");
     expect(languages(65)).toEqual("B");
     expect(languages(56)).toEqual("C");
+    expect(sciences(40)).toEqual("sup");
   });
 });
